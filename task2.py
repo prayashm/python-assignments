360deee6ec3f80f17c2bc7f785832f943bf240fd
Prayash Mohapatra <catchprayash@gmail.com>
2017-03-20 01:02:55 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,22 +1,32 @@
 import re
 
+def sentences_from(paragraph):
+    # Extract Sentences
+    # Check for delimiters: .?!
+    intermediate_para = re.sub(r'([.!?])', r'\1\n', paragraph)
+    sentences = re.split(r'[\n]\s?', intermediate_para)
+
+    # Remove last sentence if blank
+    # Happens when paragraph ends with a proper delimiter
+    if sentences[-1] == '':
+        sentences.pop()
+    
+    return sentences
+
+def tokens_from(sentences):
+    all_tokens = []
+    for i, sentence in enumerate(sentences):
+        # Add spaces before punctuation symbols
+        intermediate_text = re.sub(r"([!\"#$%&()*+,\-./:;<=>?@[\]^_`{|}~])", r' \1', sentence)
+        
+        # Tokenise the paragraph
+        tokens = intermediate_text.split()
+        all_tokens.append(tokens)
+        print('Sentence #{} has {} tokens'.format(i+1, len(tokens)))
+    
+    return all_tokens
+  
 # Take the paragraph as input
-paragraph = input()
-
-# Extract Sentences
-# Check for delimiters: .?!
-intermediate_para = re.sub(r'([.!?])', r'\1\n', paragraph)
-sentences = re.split(r'[\n]\s?', intermediate_para)
-
-# Remove last sentence if blank
-# Happens when paragraph ends with a proper delimiter
-if sentences[-1] == '':
-  sentences.pop()
-
-for i, sentence in enumerate(sentences):
-  # Add spaces before punctuation symbols
-  intermediate_text = re.sub(r"([!\"#$%&()*+,\-./:;<=>?@[\]^_`{|}~])", r' \1', sentence)
-
-  # Tokenise the paragraph
-  tokens = intermediate_text.split()
-  print('Sentence #{} has {} tokens'.format(i+1, len(tokens)))
+user_paragraph = input()
+extracted_sentences = sentences_from(user_paragraph)
+extracted_tokens = tokens_from(extracted_sentences)
